123d5b18fe721dcf00f5681bd8daa999b719935e
Toshiaki Maki <tmaki@pivotal.io>
2019-11-19 11:25:53 +0900

    change artifactId

--- gen-snapshot.sh
+++ gen-snapshot.sh
@@ -3,6 +3,6 @@ mvn org.apache.maven.plugins:maven-archetype-plugin:2.3:generate \
   -DarchetypeArtifactId=graalvm-springmvc-blank-archetype \
   -DarchetypeVersion=0.1.0-SNAPSHOT \
   -DgroupId=com.example \
-  -DartifactId=demo-cli \
+  -DartifactId=demo-tomcat \
   -Dversion=1.0.0-SNAPSHOT \
   -DarchetypeCatalog=https://oss.sonatype.org/content/repositories/snapshots
